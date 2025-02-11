c467ae3564021eed66b12d43b22146ee3fca33fb
Leonid <l.bryzgalov@kaizen.run>
2025-02-12 01:31:02 +0400

    burger menu styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/App.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import './styles/main.scss';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App />
