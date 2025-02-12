610ac4ab6cd15bbc8b19532114ebf7fcab137803
Leonid <l.bryzgalov@kaizen.run>
2025-02-12 17:11:29 +0400

    style: add new basket-hover icon and update BurgerMenu styles for better responsiveness

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,5 @@
 export { ReactComponent as Logo } from './logo.svg';
 export { ReactComponent as Basket } from './basket.svg';
+export { ReactComponent as BasketHover } from './basket-hover.svg';
+
 export { ReactComponent as Rocket } from './rocket.svg';
