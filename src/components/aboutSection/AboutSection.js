3b9e67947caa4bcd2827f3b4f293fbb0f24e19b8
Leonid <l.bryzgalov@kaizen.run>
2025-02-12 12:27:39 +0400

    style: redesign AboutSection component with new layout and styles

--- src/components/aboutSection/AboutSection.js
+++ src/components/aboutSection/AboutSection.js
@@ -1,8 +1,10 @@
+import './AboutSection.scss';
+
 export const AboutSection = () => {
     return (
-        <section>
-            <h2>Embark on a space journey</h2>
-            <p>
+        <section className="about-section">
+            <h2 className="about-section__title">Embark on a space journey</h2>
+            <p className="about-section__text">
                 Travelling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed
                 of exploring stars, planets and galaxies, then our company is ready to help you realize this dream. We offer a unique experience that will allow
                 you to go on a space journey and see all the secrets of the universe. We guarantee that every moment in space will be filled with incredible
@@ -10,7 +12,9 @@ export const AboutSection = () => {
                 adventure in space. We offer various options for space excursions.
             </p>
 
-            <button type="button">Read more</button>
+            <button type="button" className="about-section__button">
+                Read more
+            </button>
         </section>
     );
 };
