535ec6bb852fcdf1eaafa2d40b9c67115b10772a
Leonid <l.bryzgalov@kaizen.run>
2025-02-12 12:14:55 +0400

    style: redesign MainSection component with new layout and background image

--- src/components/mainSection/MainSection.js
+++ src/components/mainSection/MainSection.js
@@ -1,15 +1,23 @@
 import { Button } from '../common/button/Button';
+import earthImage from '../../assets/images/earth.png';
+
+import './MainSection.scss';
 
 export const MainSection = () => {
     return (
-        <main>
-            <h1>
-                Discover the vast expanses of <span>space</span>
-            </h1>
-            <p>
-                Where the possibilities are <span>endless!</span>
-            </p>
-            <Button>Learn more</Button>
-        </main>
+        <section className="main-section">
+            <div className="main-section__content">
+                <img src={earthImage} alt="Main Image" className="main-section__image" />
+                <h1 className="main-section__title">
+                    Discover the vast expanses of <span>space</span>
+                </h1>
+                <p className="main-section__text">
+                    Where the possibilities are <span>endless!</span>
+                </p>
+                <div className="main-section__button">
+                    <Button className={'btn--yellow'}>Learn more</Button>
+                </div>
+            </div>
+        </section>
     );
 };
